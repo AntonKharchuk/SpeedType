7d89d06c4bbd98ca2a012f16f7dc3f1c06cd2044
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-10-01 15:21:49 +0300

    chack Colsole readKey behavior

--- SpeedType.ConsoleDemo/Program.cs
+++ SpeedType.ConsoleDemo/Program.cs
@@ -1,8 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using SpeedType;
+using SpeedType.ConsoleDemo;
+
+var host = Host.CreateDefaultBuilder(args)
+           .ConfigureServices((hostContext, services) =>
+           {
+               services.AddSingleton<IUserInput, UserInput>();
+               services.AddSingleton<MainWork>();
+           })
+           .Build();
+
 Console.WriteLine("Hello, World!");
 
+var userInput = host.Services.GetRequiredService<IUserInput>();
 
-Console.WriteLine(""=="");
+var generator = new TextGenerator();
+
+var mainWork = new MainWork(generator.GetWord, userInput);
+
+mainWork.Run();
 
 Console.WriteLine("End!");
 
