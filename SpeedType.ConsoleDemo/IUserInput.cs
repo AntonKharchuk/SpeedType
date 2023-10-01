35c7c2a2bcc39012c2adcf883638c9e2229b95f3
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-10-01 10:40:17 +0300

    Add IUserInput

---
+++ SpeedType.ConsoleDemo/IUserInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedType
+{
+    public interface IUserInput
+    {
+        void ChangeInputColor(int r, int b, int g);
+    }
+}
