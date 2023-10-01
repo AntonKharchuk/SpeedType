7461e9527c6c02113aaa37b62103c3ff3b7c4d05
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-10-01 10:34:16 +0300

    Add project files.

---
+++ SpeedType/TextGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace SpeedType
+{
+    public class TextGenerator
+    {
+        private string _defaultWord;
+        private string _defaultText;
+
+        public TextGenerator()
+        {
+            _defaultWord = "Mama";
+            _defaultText = "I love mama";
+        }
+        string GetWord()
+        {
+            return _defaultWord;
+        }
+        string GetText()
+        {
+            return _defaultText;
+        }
+    }
+}
