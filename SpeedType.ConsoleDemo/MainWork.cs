239ba62f762819fe0a6a80ed68723fd5fa4e7a0b
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-10-01 15:07:16 +0300

    start version of MainWork

---
+++ SpeedType.ConsoleDemo/MainWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedType.ConsoleDemo
+{
+    internal class MainWork
+    {
+        private Func<string> _getTextToType;
+
+        private IUserInput _userInput;
+
+        public MainWork(Func<string> getTextToType, IUserInput userInput)
+        {
+            _getTextToType = getTextToType;
+
+            _userInput = userInput;
+        }
+
+        public async void Run()
+        {
+            var textToType = _getTextToType.Invoke();
+
+            Console.WriteLine(textToType);
+
+            TrackUserInput(textToType);
+        }
+
+        private async void TrackUserInput(string text)
+        {
+            string userText = string.Empty;
+
+            while (text!=userText)
+            {
+                var currentKey = Console.ReadKey(true);
+
+                if (currentKey.Key is ConsoleKey.Escape) break;
+
+                userText += currentKey.KeyChar;
+            }
+
+            Console.WriteLine(userText);
+        } 
+
+    }
+}
