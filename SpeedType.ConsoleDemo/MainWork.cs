3983ae74838039ecfce85285de9bf3733b3c2c4f
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-10-01 17:47:08 +0300

    end with TrackUserInput

--- SpeedType.ConsoleDemo/MainWork.cs
+++ SpeedType.ConsoleDemo/MainWork.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,23 +31,56 @@ namespace SpeedType.ConsoleDemo
             TrackUserInput(textToType);
         }
 
-        private async void TrackUserInput(string text)
+        private void TrackUserInput(string text)
         {
             string userText = string.Empty;
 
             text = text.Trim().ToLower();
 
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
             while (text!=userText.Trim().ToLower())
             {
                 var currentKey = Console.ReadKey(true);
 
-                if (currentKey.Key is ConsoleKey.Escape) break;
+                if (currentKey.Key is ConsoleKey.Escape) 
+                    break;
 
-                userText += currentKey.KeyChar;
+                if (currentKey.Modifiers is ConsoleModifiers.Control && currentKey.Key is ConsoleKey.Backspace)
+                    continue;
+
+                if ((currentKey.Key is ConsoleKey.Backspace))
+                {
+                    if (userText.Length>0)
+                    {
+                        userText = userText.Substring(0, userText.Length - 1);
+                    }
+                }
+                if (currentKey.Key is not ConsoleKey.Backspace)
+                {
+                    userText += currentKey.KeyChar;
+                }
+
+                if (text.StartsWith(userText.Trim().ToLower()))
+                    _userInput.ChangeInputColor(ConsoleColor.White);
+                else
+                    _userInput.ChangeInputColor(ConsoleColor.Red);
+
+                Console.Write(currentKey.KeyChar);
             }
+            Console.WriteLine();
 
-            Console.WriteLine(userText);
-        } 
+            stopwatch.Stop();
+
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+
+            Console.WriteLine($"time: {elapsedTime.Seconds}:{elapsedTime.Milliseconds}");
+            Console.WriteLine();
+
+
+        }
 
     }
 }
