5956973dd674f023ce0f833d13e20a3de8b21c95
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-10-01 17:46:01 +0300

    add 1000 common words in txt and use them in text generator

--- SpeedType/TextGenerator.cs
+++ SpeedType/TextGenerator.cs
@@ -1,22 +1,42 @@
-﻿namespace SpeedType
+﻿using System.Net.Http.Headers;
+
+namespace SpeedType
 {
     public class TextGenerator
     {
-        private string _defaultWord;
-        private string _defaultText;
+        private Random random;
+
+        private string[] _listOfWords;
 
         public TextGenerator()
         {
-            _defaultWord = "Mama";
-            _defaultText = "I love mama";
+            random = new Random();
+
+            using (var sr = new StreamReader("D:\\Code\\C#\\Learning\\SpeedType\\SpeedType\\CommonWords.txt"))
+            {
+                _listOfWords = sr.ReadToEnd().Split(new[]{'\n','\r'}, StringSplitOptions.RemoveEmptyEntries);
+            }
         }
         public string GetWord()
         {
-            return _defaultWord;
+            int index = random.Next(1000);
+
+            if (index < _listOfWords.Length)
+                return _listOfWords[index];
+            return _listOfWords[100];
         }
         public string GetText()
         {
-            return _defaultText;
+            string result = string.Empty;
+
+            int index;
+
+            for (int i = 0; i < 10; i++)
+            {
+                index = random.Next(1000);
+                result += (_listOfWords[index]+" ");
+            }
+            return result;
         }
     }
 }
